28259f3051b767bffd2bf0b0ffd04aff2b3637d0
DiogoCatalao <writerd2013@hotmail.com>
2019-04-27 20:02:59 +0100

    image and button

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -87,7 +87,7 @@
                     {{-- Magic --}}
                 </div>
 
-                <div class="links">
+                {{-- <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
@@ -95,7 +95,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                </div> --}}
             </div>
         </div>
     </body>
